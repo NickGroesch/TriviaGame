7e862070a99fa0364f9cc4f2f5e6dbb19929a45d
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-03-10 14:05:00 -0500

    working game ready to deploy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var trivia = [
-    { Q: "Principia Mathematic is a formally complete system.", A: false },
+    { Q: "Principia Mathematica is a formally complete system.", A: false },
     {
       Q:
         "Formal incompleteness means that a system of true or false statements must have some undecidable value.",
@@ -8,24 +8,26 @@ $(document).ready(function() {
     },
     {
       Q:
-        "The writer of these questions clearly knows about what he is talking.",
+        "The writer of these questions clearly knows everything about what he is talking about.",
       A: false
     },
     {
       Q:
-        "'Yields falsehood when proceeded by it's quotation' yields falsehood when proceeded by it's quotation.",
+        "'Yields falsehood when proceeded by its quotation' yields falsehood when proceeded by its quotation.",
       A: true
     },
     {
       Q:
-        "Epimenides proposed the paradox wherein you cannot believe some guy who says 'I am always lying.'",
+        "Epimenides is attributed with the paradox wherein you cannot take at face value some character who says 'I am always lying.'",
       A: true
-    }
+    },
+    { Q: "Principia Mathematica was written by Isaac Newton", A: false },
+    { Q: "An inconsistent theory could prove its own consistency.", A: true }
   ];
   // create start button
   var start = $("<button>");
   start.addClass("start");
-  start.text("PRESS START");
+  start.text("START");
   $(".game").append(start);
   // sets the countdown
   var time = 121;
@@ -77,8 +79,6 @@ $(document).ready(function() {
   // this function runs the clock
   function clock() {
     time--;
-    console.log(time);
-
     var dispTime = minsec(time);
     $(".time").text(dispTime);
     if (time == 0) {
@@ -117,15 +117,18 @@ $(document).ready(function() {
     for (i = 0; i < trivia.length; i++) {
       var Qid = "id" + i;
       var Aid;
+      // make sure the player has answered all the questions if time is not up, if not sends them back to the game with clock restarted
       if (!completed(Qid) && !late) {
         $(".game").prepend("<h4>Finish the game!</h4>");
         countDown = setInterval(clock, 1000);
         return;
+        // if player has not answered but time is up grades them anyway by adding blank wherever they failed to answer
       } else if (!completed(Qid)) {
         Aid = `blank${i}`;
       } else {
         Aid = $("input:radio:checked")[i].id;
       }
+      // slices the last digit of boolean[i] off to facilitate grading
       playerA.push(Aid.slice(0, -1));
     }
     // get the answer values from the trivia array
@@ -134,8 +137,6 @@ $(document).ready(function() {
       var qA = trivia[i].A;
       triviaA.push(qA.toString());
     }
-    console.log(triviaA);
-    console.log(playerA);
     // now we compare the arrays to find if each element matches, and display it
     $(".game").empty();
     var rights = 0;
@@ -149,17 +150,23 @@ $(document).ready(function() {
         wrongs++;
       }
     }
-    // lately we discovered we prefer template literals
+    // lately we've discovered we prefer template literals;
     var rightsCount = `You got ${rights} questions right!`;
     var wrongsCount = `You got ${wrongs} questions wrong!`;
     $(".game").append(`<br><p>${rightsCount}</p><p>${wrongsCount}</p>`);
+    // creates a restart button
+    $(".game").append(
+      $("<button>")
+        .addClass("restart")
+        .text("Try Again")
+    );
   }
-
   //   handle button clicks- start doesn't need document prefix because it is generated on load, rather than later
   //  this way we logically order our game to call the functions defined above
   $(".start").on("click", startIt);
   $(document).on("click", ".submit", gradeIt);
-  //   function() {
-  //   if(completed()) all questions are answered
-  // });
+  $(document).on("click", ".restart", function() {
+    time = 121;
+    startIt();
+  });
 });
